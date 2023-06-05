33766f79eccdb1e8251f804f124e5fa35bcc4bbb
adityamaulana <aditmau26@gmail.com>
2023-06-05 10:44:15 +0700

    first commm

--- exercisekedua_040/exercisekedua_040.cpp
+++ exercisekedua_040/exercisekedua_040.cpp
@@ -1,20 +1,73 @@
-// exercisekedua_040.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+class Penerbit
+{
+	public:
+		string Name;
+
+		void printAll();
+
+		Penerbit(string pName) :
+			Name(pName)
+		{
+			cout << "Daftar Pengarang Pada Penerbit Tempo" << Name << "\n" << endl;
+
+			cout << "Daftar Pengarang Pada Penerbit NYTimes" << Name << "\n" << endl;
+		}
+
+		Penerbit(string pName) :Name(pName) {}
+};
+
+class Pengarang : virtual public Penerbit
+{
+	public:
+
+		Pengarang(string pName) :
+			Penerbit(pName)
+		{
+			cout << "Daftar Penerbit yang Diikuti J.K Rowling\n" << endl;	
+		}
+};
+
+class Book : virtual public Penerbit
+{
+	public:
+		Book(string pName) :
+			Penerbit(pName)
+		{
+			cout << "Daftar Buku yang dikarang J.K Rowling\n" << endl;
+			cout << "Daftar Buku yang dikarang Tere Liye\n" << endl;
+			cout << "Daftar Buku yang dikarang Jonstein Gardnerr\n" << endl;
+			cout << "Daftar Buku yang dikarang Andrea Hirata\n" << endl;
+		}
+
+};
+class DPenerbit : public Pengarang, public Book
+{
+	public:
+
+		DPenerbit(string pName) :
+			Penerbit(pName),
+			Pengarang(pName),
+			Book(pName)
+		{
+			cout << "Daftar Penerbit\n" << endl;
+		}
+};
+
+void Penerbit::printAll()
+{
+	cout << "Daftar Penerbit = " << Name << endl;
+
+	cout << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Penerbit a();
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
